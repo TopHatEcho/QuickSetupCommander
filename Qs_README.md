a63f31e66546395793d995583e73df115ba0d633
TopHatEcho <george.wallin@appliedtechnology.se>
2018-09-25 21:09:12 +0200

    Mk1

---
+++ Qs_README.md
@@ -0,0 +1,31 @@
+# QuickSetupCommander
+
+
+Hello and thank you for using Quick Setup Commander!!
+
+First it will run 'npm init' followed by the dependencies listed below followed by the eslint init
+
+*NOTE this file does not edit the eslintrc "YET" so edit that to your preferences
+
+My perfered settings for eslintrc
+Add mocha to true
+add mocha to plugins
+set indent to 2
+add SwitchCase: 1
+and a "no-console": 0
+
+
+This Program will install the folowing Dependecies as Dev-dependecies
+- eslint
+- eslint-config-airbnb-base
+- eslint-plugin-import
+- eslint-plugin-mocha
+- jsdom
+- mocha
+- nodemon
+- onchange
+- wdio-mocha-framework
+- wdio-spec-reporter
+- node-fetch
+- axios
+-archiver
---
+++ QuickSetup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+while true; do
+    read -p "Have you run 'npm init' ? (Y/N)" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) echo "Please answer the questions provided by 'npm init'" 
+        wait
+        npm init
+        wait
+        break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+wait
+npm install eslint --save-dev &
+wait
+npm install eslint-config-airbnb-base --save-dev &
+wait
+npm install eslint-plugin-mocha --save-dev &
+wait
+npm install eslint-plugin-import --save-dev &
+wait
+npm install nodemon --save-dev &
+wait
+npm install archiver --save-dev &
+wait
+npm install axios --save-dev &
+wait
+npm install node-fetch --save-dev &
+wait
+npm install jest --save-dev &
+wait
+npm install mocha --save-dev &
+wait
+npm install onchange --save-dev &
+wait
+npm install jsdom --save-dev &
+wait
+npm install wdio-mocha-framework --save-dev &
+wait
+npm install wdio-spec-reporter --save-dev &
+wait
+echo "please enter preferd linting options"
+wait
+node ./node_modules/.bin/eslint --init
+
+
+
+
+wait
+echo 'Finished! Thank you for using Quicksetup Dont forget to edit the eslint rc'
